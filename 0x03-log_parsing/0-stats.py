0d02b6e213359cc9b938b428f77727456fb39958
Yeshiwas Dagnaw <yeshiwasdagnaw@gmail.com>
2024-03-28 15:01:05 +0300

    fixed

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-"""a script that reads stdin line by line and computes metrics:
-"""
-
+"""A script that reads stdin line by line and computes metrics"""
 
 import sys
 
-# store the count of all status codes in a dictionary
+# Store the count of all status codes in a dictionary
 status_codes_dict = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
                      '404': 0, '405': 0, '500': 0}
 
 total_size = 0
-count = 0  # keep count of the number lines counted
+count = 0  # Keep count of the number of lines counted
 
 try:
     for line in sys.stdin:
@@ -20,22 +18,21 @@ try:
             status_code = line_list[-2]
             file_size = int(line_list[-1])
 
-            # check if the status code receive exists in the dictionary and
-            # increment its count
-            if status_code in status_codes_dict.keys():
+            # Check if the status code received exists in the dictionary and increment its count
+            if status_code in status_codes_dict:
                 status_codes_dict[status_code] += 1
 
-            # update total size
+            # Update total size
             total_size += file_size
 
-            # update count of lines
+            # Update count of lines
             count += 1
 
         if count == 10:
-            count = 0  # reset count
-            print('File size: {}'.format(total_size))
+            count = 0  # Reset count
+            print('File size:', total_size)
 
-            # print out status code counts
+            # Print out status code counts
             for key, value in sorted(status_codes_dict.items()):
                 if value != 0:
                     print('{}: {}'.format(key, value))
@@ -44,7 +41,8 @@ except Exception as err:
     pass
 
 finally:
-    print('File size: {}'.format(total_size))
+    # Print final statistics
+    print('File size:', total_size)
     for key, value in sorted(status_codes_dict.items()):
         if value != 0:
-            print('{}: {}'.format(key, value))
+            print('{}: {}'.format(key, value)))
