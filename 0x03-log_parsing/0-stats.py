2ba278293643cb026e71555ac532b7b93690e5cf
Yeshiwas Dagnaw <yeshiwasdagnaw@gmail.com>
2024-03-28 14:56:44 +0300

    fixed

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,84 +1,37 @@
 #!/usr/bin/python3
-"""
-Read stdin line by line and computes metrics
-Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1"
-<status code> <file size>, skip line if not this format
-After every 10 minutes or keyboard interrupt (CTRL + C)
-print these from beginning: number of lines by status code
-possible status codes: 200, 301, 400, 401, 404, 405, and 500
-if status code isn't an integer, do not print it
-format: <status code>: <number>
-Status code must be printed in ascending order
-"""
+'''a script that reads stdin line by line and computes metrics'''
+
+
 import sys
-import time
 
-def print_statistics(status_counts, total_file_size):
-    print("File size:", total_file_size)
-    for code in sorted(status_counts):
-        if status_counts[code] > 0:
-            print("{}: {}".format(code, status_counts[code]))
-
-status_counts = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0
-}
-total_file_size = 0
-start_time = time.time()
-lines_processed = 0
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
+total_size = 0
+counter = 0
 
 try:
     for line in sys.stdin:
-        # Parse the line
-        parts = line.strip().split()
-        if len(parts) != 7:
-            continue
+        line_list = line.split(" ")
+        if len(line_list) > 4:
+            code = line_list[-2]
+            size = int(line_list[-1])
+            if code in cache.keys():
+                cache[code] += 1
+            total_size += size
+            counter += 1
 
-        status_code = parts[-2]
-        file_size = int(parts[-1])
+        if counter == 10:
+            counter = 0
+            print('File size: {}'.format(total_size))
+            for key, value in sorted(cache.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
 
-        # Update metrics
-        if status_code.isdigit() and status_code in status_counts:
-            status_counts[status_code] += 1
-            total_file_size += file_size
-            lines_processed += 1
+except Exception as err:
+    pass
 
-        # Check if 10 minutes have elapsed
-        if time.time() - start_time >= 600:
-            print_statistics(status_counts, total_file_size)
-            start_time = time.time()
-            lines_processed = 0
-            total_file_size = 0
-            status_counts = {
-                "200": 0,
-                "301": 0,
-                "400": 0,
-                "401": 0,
-                "404": 0,
-                "405": 0,
-                "500": 0
-            }
-
-        # Check if 10 lines have been processed
-        if lines_processed == 10:
-            print_statistics(status_counts, total_file_size)
-            lines_processed = 0
-            total_file_size = 0
-            status_counts = {
-                "200": 0,
-                "301": 0,
-                "400": 0,
-                "401": 0,
-                "404": 0,
-                "405": 0,
-                "500": 0
-            }
-
-except KeyboardInterrupt:
-    # Print final statistics if interrupted
-    print_statistics(status_counts, total_file_size)
+finally:
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(cache.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
